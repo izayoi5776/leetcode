bb3eefd083a70f97406e6e0c1907680f3b9a1cfb
zxd <izayoi5776@gmail.com>
2020-03-12 00:32:56 +0900

    将数组分成和相等的三个部分
    マイナスもありだから、結局全部計算しないと分からない。

--- 1013.java
+++ 1013.java
@@ -5,7 +5,6 @@ https://leetcode-cn.com/problems/partition-array-into-three-parts-with-equal-sum
 
 *******************************************************************************/
 import java.util.stream.*;
-import java.util.Arrays;
 
 class Solution {
     public boolean canThreePartsEqualSum(int[] A) {
@@ -19,49 +18,25 @@ class Solution {
             int target = sum / 3; // 部分合計
             
             // 位置１探す
-            int p1 = A.length /3; // 位置１の開始は1/3地点
-            int sum1 = IntStream.of(Arrays.copyOfRange(A, 0, p1)).sum(); // p1 まで合計
-            if(sum1 > target){
-                while(sum1 > target && p1 > 0 ){
-                    int n = A[p1];
-                    p1 -= 1;
-                    sum1 -= n;
-                }
-                if(sum1 != target){
-                    return false;
-                }
-            }else if(sum1 < target){
-                while(sum1 < target && p1 < A.length -2 ){
-                    int n = A[p1];
-                    p1 += 1;
-                    sum1 += n;
-                }
-                if(sum1 != target){
-                    return false;
-                }
+            int p1 = 1; // 位置１
+            int sum1 = A[0]; // p1 まで合計
+            while(sum1 != target && p1 < A.length -2 ){
+                sum1 += A[p1];;
+                p1 += 1;
+            }
+            if(sum1 != target){
+                return false;
             }
             
             // 位置２探す
-            int p2 = (p1 + A.length) / 2; // 位置２の初期時点
-            int sum2 = IntStream.of(Arrays.copyOfRange(A, p2, A.length)).sum(); // p2以降の合計
-            if(sum2 > target){
-                while(sum2 > target && p2 < A.length - 2 ){
-                    int n = A[p2];
-                    p2 += 1;
-                    sum2 -= n;
-                }
-                if(sum2 != target){
-                    return false;
-                }
-            }else if(sum2 < target){
-                while(sum2 < target && p1 < A.length -2 ){
-                    int n = A[p2];
-                    p2 += 1;
-                    sum1 += n;
-                }
-                if(sum1 != target){
-                    return false;
-                }
+            int p2 = p1 + 1; // 位置２の初期時点
+            int sum2 = A[p1]; // p2以降の合計
+            while(sum2 != target && p2 < A.length -1 ){
+                sum2 += A[p2];
+                p2 += 1;
+            }
+            if(sum2 != target){
+                return false;
             }
         }
         return true;
@@ -75,6 +50,6 @@ public class Main
 		int[] b = {0,2,1,-6,6,-7,9,1,2,0,1}; // true
 
         Solution o = new Solution();
-        System.out.println(o.canThreePartsEqualSum(b));
+        System.out.println(o.canThreePartsEqualSum(a));
 	}
 }
