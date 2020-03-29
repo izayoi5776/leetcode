2e6df473413423bdf6a002970bb0cfbf7784758a
zxd <izayoi5776@gmail.com>
2020-03-29 15:02:09 +0900

    加入各种简便工具 TreeNode

---
+++ 00_commons.java
@@ -0,0 +1,94 @@
+class TreeNode {
+    int val;
+    boolean nullflg = false;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() { nullflg=true; }
+    TreeNode(int x) { val = x; }
+    TreeNode(int x, TreeNode lt, TreeNode rt) {
+        val = x;
+        left = lt;
+        right = rt;
+    }
+	/**
+	 * 通过输入的配列初始化一棵二叉树
+	 * 例 tree = [7,4,3,null,null,6,19]
+	 * 配列是对树的广度搜索，从上到下，从左到右，空分支用null
+	 */
+	TreeNode(Integer[] itree){
+	    System.out.println("buildTree() itree=" + Arrays.toString(itree));
+	    if(itree.length>0){
+    	    Queue<TreeNode> todo = new ArrayDeque<TreeNode>();
+    	    val = itree[0];
+    	    int pos = 1; // 指示在 itree 中的位置
+    	    todo.add(this);
+    	    while(todo.size()>0){
+    	        TreeNode cnd = todo.remove(); // 现在处理中的 node
+    	        Integer cval = 0;
+    	        cval = getN(itree, pos);
+    	        // left
+    	        if(cval==null){
+    	            // cnd.left = null;
+    	        }else{
+    	            TreeNode tmp = new TreeNode(cval);
+    	            cnd.left = tmp;
+                    todo.add(tmp);
+    	        }
+    	        pos++;
+    	        cval = getN(itree, pos);
+
+    	        // right
+    	        if(cval==null){
+    	            // cnd.right = null;
+    	        }else{
+    	            TreeNode tmp = new TreeNode(cval);
+    	            cnd.right = tmp;
+                    todo.add(tmp);
+    	        }
+    	        pos++;
+    	        System.out.println("tree=" + this);
+    	    }
+	    }
+	}
+	// get itree[pos], return null if invalid
+	private static Integer getN(Integer[] itree, int pos){
+	    Integer ret = null;
+	    if(itree==null || pos < 0 || pos >= itree.length){
+	        // ret = null;
+	    }else{
+	        ret = itree[pos];
+	    }
+	    return ret;
+	}
+	// 广度优先打印
+    public String toString(){
+        Queue<TreeNode> todo = new ArrayDeque<TreeNode>();
+        String ret = "";
+        todo.add(this);
+
+        while(todo.size()>0){
+            TreeNode cnd = todo.remove(); // current node
+            if(cnd.nullflg){
+                ret += "null,";
+            }else{
+                ret += cnd.val + ",";
+            }
+            if(cnd.left==null && cnd.right==null){
+                // do nothing
+            }else{
+                if(cnd.left==null){
+                    todo.add(new TreeNode());
+                }else{
+                    todo.add(cnd.left);
+                }
+                if(cnd.right==null){
+                    todo.add(new TreeNode());
+                }else{
+                    todo.add(cnd.right);
+                }
+            }
+        }
+
+        return ret;
+    }	
+}
