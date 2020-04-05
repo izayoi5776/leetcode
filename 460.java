5166d0769ed265bcb690a9a1c0c37836045298bc
zxd <izayoi5776@gmail.com>
2020-04-05 15:58:28 +0900

    双链表。
    测试6通不过。

--- 460.java
+++ 460.java
@@ -6,43 +6,143 @@ https://leetcode-cn.com/problems/lfu-cache/
 *******************************************************************************/
 import java.util.Arrays;
 import java.util.Map;
-import java.util.LinkedHashMap;
+import java.util.HashMap;
+//import java.util.LinkedHashMap;
 
-class MyMap<E, F> extends LinkedHashMap<E, F>{
-    int cap = 100;
-    MyMap(int capacity){
-        super(capacity, 0.75f, true);
-        cap = capacity;
-        //System.out.println("MyMap() called");
+class Rec{
+    int key;
+    int value;
+    Rec prev;
+    Rec next;
+    Rec(int key, int value){
+        this.key = key;
+        this.value = value;
     }
-    protected boolean removeEldestEntry(Map.Entry eldest) {
-        boolean ret = size() > cap;
-        System.out.println("MyMap::removeEldestEntry() called eldest=[" + eldest + "] size=" + size() + " ret=" + ret);
+    String printTree(){
+        String ret = "" + key + "=" + value;
+        if(next!=null){
+            ret += "/" + next.printTree();
+        }
         return ret;
     }
 }
-
 class LFUCache {
-    MyMap<Integer, Integer> m = null;
+    Map<Integer, Rec> m = new HashMap<Integer, Rec>();
+    int capacity = 0;
+    Rec head = null;    // 真的开头
+    Rec tail = null;    // 结尾
+    Rec ins  = null;    // 插入点
 
     public LFUCache(int capacity) {
-        m = new MyMap<Integer, Integer>(capacity);
+        this.capacity = capacity;
         //System.out.println("LFUCache() called cap=" + capacity + " m=" + m);
     }
     
     public int get(int key) {
         int ret = -1;
-        Integer val = m.get(key);
-        if(val!=null){
-            ret = val;
-        };
-        //System.out.println("LFUCache::get() called key=" + key + " ret=" + ret);
+        if(m.containsKey(key)){
+            Rec r = m.get(key);
+            if(head!=r){
+                Rec rp = r.prev;
+                Rec rn = r.next;
+                if(rp!=null){rp.next = rn;}
+                if(rn!=null){rn.prev = rp;}
+
+                if(ins==r){
+                    ins=rn;
+                }    
+                if(tail==r){
+                    tail=rp;
+                }
+
+                r.next = head;
+                r.prev = null;
+                head.prev = r;
+                head = r;
+            }else{
+                if(ins==r){
+                    ins = r.next;    
+                }
+            }
+            ret = r.value;
+        }
+        printStatus("LFUCache::get(" + key + ")=" + ret);
         return ret;
     }
-    
+
     public void put(int key, int value) {
-        m.put(key, value);
-        System.out.println("LFUCache::put() called key=" + key + " value=" + value + " m=" + m);
+        if(m.containsKey(key)){
+            Rec r = m.get(key);
+            r.value = value;
+            m.put(key, r);
+        }else{
+            if(chkCap()){
+                Rec r = new Rec(key, value);
+                if(head==null){
+                    head = r;
+                }
+                if(tail==null){
+                    tail = r;
+                }
+                if(ins==null){
+                    ins = r;    // 指向最新插入的记录
+                    if(tail!=null && tail != r){
+                        tail.next = r;
+                        r.prev = tail;
+                    }
+                    tail = r;
+                }else{
+                    Rec rp = ins.prev;
+                    if(rp!=null){rp.next = r;}
+                    r.prev = rp;
+    
+                    if(head==ins){
+                        head = r;
+                    }
+    
+                    ins.prev = r;
+                    r.next = ins;
+    
+                    ins = r;
+                }
+                m.put(key, r);
+            }
+        }
+        printStatus("LFUCache::put(" + key + "," + value + ")");
+    }
+    boolean chkCap(){
+        if(m.size() > capacity -1){
+            if(tail!=null){
+                printStatus("LFUCache::chkCap() size()=" + m.size() + ">" + (capacity - 1) + " REMOVE ");
+                m.remove(tail.key);
+                if(head==tail){
+                    head = null;
+                    tail = null;
+                    ins = null;
+                }else{
+                    if(ins==tail){
+                        ins = null;
+                    }
+                    if(tail.prev!=null){
+                        tail = tail.prev;
+                        tail.next = null;
+                    }
+                }
+            }
+        }
+        printStatus("LFUCache::chkCap()");
+        return m.size() < capacity;
+    }
+    void printStatus(String s){
+        if(true){
+            String shead = null;
+            String sins = null;
+            String stail = null;
+            if(head!=null){shead = head.printTree();}
+            if(ins!=null){sins = ins.printTree();}
+            if(tail!=null){stail = tail.printTree();}
+            System.out.println(s + " keys=" + m.keySet() + " head=" + shead + " ins=" + sins + " tail=" + stail);
+        }
     }
 }
 
@@ -56,11 +156,15 @@ class LFUCache {
 public class Main
 {
 	public static void main(String[] args) {
-		//t1();
+		/*t1();
 		t2();
+		t3();
+		t4();
+		t5(); */
+		t6();
 	}
     static void tbase(LFUCache cache, int val, int expect){
-	    System.out.println("ret=" + val + " expect=" + expect);
+	    System.out.println("ret=" + val + " expect=" + expect +  (val==expect?" OK":" NG"));
 	}
 	static void t1(){
         LFUCache cache = new LFUCache( 2 /* capacity (缓存容量) */ );
@@ -88,10 +192,45 @@ public class Main
 	        }
 	    }
 	}
+	// 注意，put 不计算为使用，所以get(3)得到-1。
+	// 所以不能用 LinkedHashMap 了
 	static void t2(){
+	    System.out.println("---------------");
 	    tbase2(new String[]{"LFUCache","put","put","get","get","get","put","put","get","get","get","get"},
 	        new int[][]{{3},{2,2},{1,1},{2},{1},{2},{3,3},{4,4},{3},{2},{1},{4}},
 	        new Integer[]{null,null,null,2,1,2,null,null,-1,2,1,4}
         );
 	}
+	// put() 并不是在最后，而是按顺序。所以同是没有get过的2和3，较老的3被删掉。
+	static void t3(){
+	    System.out.println("---------------");
+	    tbase2(new String[]{"LFUCache","put","put","put","put","get"},
+	        new int[][]{{2},{3,1},{2,1},{2,2},{4,4},{2}},
+	        new Integer[]{null,null,null,null,null,2}
+        );
+	}
+	// cap == 0 时
+	static void t4(){
+	    System.out.println("---------------");
+	    tbase2(new String[]{"LFUCache","put","get"},
+	        new int[][]{{0},{0,0},{0}},
+	        new Integer[]{null,null,-1}
+        );
+	}
+
+	static void t5(){
+	    System.out.println("---------------");
+	    tbase2(new String[]{"LFUCache","put","put","get","put","put","get"},
+	        new int[][]{{2},{2,1},{2,2},{2},{1,1},{4,1},{2}},
+	        new Integer[]{null,null,null,2,null,null,2}
+        );
+	}
+	static void t6(){
+	    System.out.println("---------------");
+	    tbase2(new String[]{"LFUCache","put","put","put","put","get","get"},
+	        new int[][]{{2},{2,1},{1,1},{2,3},{4,1},{1},{2}},
+	        new Integer[]{null,null,null,null,null,-1,3}
+        );
+	}
+    
 }
