057b480d2a7de559be40b514cf089e0624f98516
zxd <izayoi5776@gmail.com>
2020-04-05 17:58:37 +0900

    t7 通不过。可能是相同项值变更时候要算没get过的

--- 460.java
+++ 460.java
@@ -12,6 +12,7 @@ import java.util.HashMap;
 class Rec{
     int key;
     int value;
+    boolean getflg = false;  // get flag
     Rec prev;
     Rec next;
     Rec(int key, int value){
@@ -19,7 +20,7 @@ class Rec{
         this.value = value;
     }
     String printTree(){
-        String ret = "" + key + "=" + value;
+        String ret = "(" + key + "," + value + "," + (getflg?"T":"F") + "," + (prev==null?"N":prev.key) + "," + (next==null?"N":next.key) +")";
         if(next!=null){
             ret += "/" + next.printTree();
         }
@@ -42,6 +43,7 @@ class LFUCache {
         int ret = -1;
         if(m.containsKey(key)){
             Rec r = m.get(key);
+            r.getflg = true;
             if(head!=r){
                 Rec rp = r.prev;
                 Rec rn = r.next;
@@ -73,8 +75,36 @@ class LFUCache {
     public void put(int key, int value) {
         if(m.containsKey(key)){
             Rec r = m.get(key);
-            r.value = value;
-            m.put(key, r);
+            if(r.value==value){
+                // 值不变不需要追加
+            }else{
+                r.getflg = false;
+                r.value = value;
+                m.put(key, r);
+
+                if(head==r){
+                    // do nothing
+                }else{
+                    if(!r.getflg){
+                        if(ins != r){
+                            Rec rp = r.prev;
+                            Rec rn = r.next;
+                            if(rp!=null){rp.next = rn;}
+                            if(rn!=null){rn.prev = rp;}
+                            if(tail==r){tail = rp;}
+                            if(head==ins){head=r;}
+        
+                            r.next = ins;
+                            r.prev = ins.prev;
+                            if(ins.prev!=null){
+                                ins.prev.next=r;
+                            }
+                            ins.prev = r;
+                            ins = r;
+                        }
+                    }
+                }
+            }
         }else{
             if(chkCap()){
                 Rec r = new Rec(key, value);
@@ -133,6 +163,27 @@ class LFUCache {
         printStatus("LFUCache::chkCap()");
         return m.size() < capacity;
     }
+    // 删除一项
+    void remove(int key){
+        if(m.containsKey(key)){
+            Rec r = m.get(key);
+            m.remove(r);
+            if(head==r){
+                head = r.next;
+            }
+            if(ins==r){
+                ins = r.next;
+            }
+            if(tail==r){
+                tail = r.prev;
+            }
+            
+            Rec rp = r.prev;
+            Rec rn = r.next;
+            if(rp!=null){rp.next = rn;}
+            if(rn!=null){rn.prev = rp;}
+        }
+    }
     void printStatus(String s){
         if(true){
             String shead = null;
@@ -156,12 +207,13 @@ class LFUCache {
 public class Main
 {
 	public static void main(String[] args) {
-		/*t1();
+		t1();
 		t2();
 		t3();
 		t4();
-		t5(); */
+		t5(); 
 		t6();
+		t7();
 	}
     static void tbase(LFUCache cache, int val, int expect){
 	    System.out.println("ret=" + val + " expect=" + expect +  (val==expect?" OK":" NG"));
@@ -180,6 +232,7 @@ public class Main
         tbase(cache, cache.get(4), 4);       // 返回 4
 	}
 	static void tbase2(String[] cmds, int[][] parms, Integer[] expects){
+	    System.out.println("---------------");
         LFUCache cache = null;
 	    for(int i=0; i<cmds.length; i++){
 	        String c = cmds[i];
@@ -190,12 +243,17 @@ public class Main
 	        }else if(c.equals("get")){
 	            tbase(cache, cache.get(parms[i][0]), expects[i]);
 	        }
+	        try{
+	            //System.in.read();    
+	        }catch(Exception ex){
+	            
+	        }
+	        
 	    }
 	}
 	// 注意，put 不计算为使用，所以get(3)得到-1。
 	// 所以不能用 LinkedHashMap 了
 	static void t2(){
-	    System.out.println("---------------");
 	    tbase2(new String[]{"LFUCache","put","put","get","get","get","put","put","get","get","get","get"},
 	        new int[][]{{3},{2,2},{1,1},{2},{1},{2},{3,3},{4,4},{3},{2},{1},{4}},
 	        new Integer[]{null,null,null,2,1,2,null,null,-1,2,1,4}
@@ -203,7 +261,6 @@ public class Main
 	}
 	// put() 并不是在最后，而是按顺序。所以同是没有get过的2和3，较老的3被删掉。
 	static void t3(){
-	    System.out.println("---------------");
 	    tbase2(new String[]{"LFUCache","put","put","put","put","get"},
 	        new int[][]{{2},{3,1},{2,1},{2,2},{4,4},{2}},
 	        new Integer[]{null,null,null,null,null,2}
@@ -211,7 +268,6 @@ public class Main
 	}
 	// cap == 0 时
 	static void t4(){
-	    System.out.println("---------------");
 	    tbase2(new String[]{"LFUCache","put","get"},
 	        new int[][]{{0},{0,0},{0}},
 	        new Integer[]{null,null,-1}
@@ -219,17 +275,21 @@ public class Main
 	}
 
 	static void t5(){
-	    System.out.println("---------------");
 	    tbase2(new String[]{"LFUCache","put","put","get","put","put","get"},
 	        new int[][]{{2},{2,1},{2,2},{2},{1,1},{4,1},{2}},
 	        new Integer[]{null,null,null,2,null,null,2}
         );
 	}
 	static void t6(){
-	    System.out.println("---------------");
 	    tbase2(new String[]{"LFUCache","put","put","put","put","get","get"},
 	        new int[][]{{2},{2,1},{1,1},{2,3},{4,1},{1},{2}},
 	        new Integer[]{null,null,null,null,null,-1,3}
+        );
+	}
+	static void t7(){
+	    tbase2(new String[]{"LFUCache","put","put","put","put","put","get","put","get","get","put","get","put","put","put","get","put","get","get","get","get","put","put","get","get","get","put","put","get","put","get","put","get","get","get","put","put","put","get","put","get","get","put","put","get","put","put","put","put","get","put","put","get","put","put","get","put","put","put","put","put","get","put","put","get","put","get","get","get","put","get","get","put","put","put","put","get","put","put","put","put","get","get","get","put","put","put","get","put","put","put","get","put","put","put","get","get","get","put","put","put","put","get","put","put","put","put","put","put","put"},
+	        new int[][]{{10},{10,13},{3,17},{6,11},{10,5},{9,10},{13},{2,19},{2},{3},{5,25},{8},{9,22},{5,5},{1,30},{11},{9,12},{7},{5},{8},{9},{4,30},{9,3},{9},{10},{10},{6,14},{3,1},{3},{10,11},{8},{2,14},{1},{5},{4},{11,4},{12,24},{5,18},{13},{7,23},{8},{12},{3,27},{2,12},{5},{2,9},{13,4},{8,18},{1,7},{6},{9,29},{8,21},{5},{6,30},{1,12},{10},{4,15},{7,22},{11,26},{8,17},{9,29},{5},{3,4},{11,30},{12},{4,29},{3},{9},{6},{3,4},{1},{10},{3,29},{10,28},{1,20},{11,13},{3},{3,12},{3,8},{10,9},{3,26},{8},{7},{5},{13,17},{2,27},{11,15},{12},{9,19},{2,15},{3,16},{1},{12,17},{9,1},{6,19},{4},{5},{5},{8,1},{11,7},{5,2},{9,28},{1},{2,2},{7,4},{4,22},{7,24},{9,26},{13,28},{11,26}},
+	        new Integer[]{null,null,null,null,null,null,-1,null,19,17,null,-1,null,null,null,-1,null,-1,5,-1,12,null,null,3,5,5,null,null,1,null,-1,null,30,5,30,null,null,null,-1,null,-1,24,null,null,18,null,null,null,null,14,null,null,18,null,null,11,null,null,null,null,null,18,null,null,-1,null,4,29,30,null,12,11,null,null,null,null,29,null,null,null,null,17,-1,18,null,null,null,-1,null,null,null,20,null,null,null,29,18,18,null,null,null,null,20,null,null,null,null,null,null,null}
         );
 	}
     
