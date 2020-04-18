a14fb7717f540d634164cd2f07cc2266abc56fa1
zxd <izayoi5776@gmail.com>
2020-04-18 13:59:15 +0900

    不同的二叉搜索树 II (太慢不行版)
    n=7失败。
    因为开始搞全搜索，然后在基础上过滤，所以太慢。只好重写。

--- 1233.java
+++ 1233.java
@@ -15,34 +15,33 @@ import java.util.ArrayDeque;
 class Rec{
     List<Integer> ls = new ArrayList<>();
     List<Integer> rem  = new ArrayList<>();
-    boolean left = false; // true=LEFT
+    int nullcount = 0; // 最大n+1个null
+    //boolean left = false; // true=LEFT
     Rec(int n, List<Integer> rem){
         ls.add(n);
         this.rem = rem;
+        this.nullcount = rem.size();
     }
-    Rec(){}
+    Rec(int n){nullcount=n;}
     // 复制 ls 和 rem，并把 n 从 rem 中移到 ls 里面
     Rec next(Integer n){
-        Rec ret = new Rec();
+        Rec ret = new Rec(nullcount);
         ret.ls = new ArrayList<>(ls);
         ret.ls.add(n);
         ret.rem = new ArrayList<>(rem);
-        if(n!=null){
+        if(n==null){
+            ret.nullcount--;
+            if(nullcount<0){
+                ret = null;
+            }
+        }else{
             ret.rem.remove(n);
         }
-        ret.left = !left;
-        return ret;
-    }
-    boolean isLeftNull(){
-        boolean ret = false;
-        if(left && ls.get(ls.size()-1)==null){
-            ret = true;
-        }
-        //System.out.println("isLeftNull() " + this + " ret=" + ret + " left=" + left + " ls.get(ls.size()-1)=" + ls.get(ls.size()-1));
+        //ret.left = !left;
         return ret;
     }
     public String toString(){
-        return "" + ls + "_" + (left?"L":"R") + "_" + rem;
+        return "" + ls + "_" + nullcount + "_" + rem;
     }
 	/**
 	 * 通过输入的配列初始化一棵二叉树
@@ -52,7 +51,7 @@ class Rec{
 	TreeNode getTree(){
 	    Integer[] itree = ls.toArray(new Integer[ls.size()]);
 	    TreeNode ret = null;
-	    System.out.println("buildTree() itree=" + Arrays.toString(itree));
+	    //System.out.println("buildTree() itree=" + Arrays.toString(itree));
 	    if(itree.length>0){
     	    Queue<TreeNode> todo = new ArrayDeque<TreeNode>();
     	    ret = new TreeNode(itree[0]);
@@ -82,9 +81,10 @@ class Rec{
                     todo.add(tmp);
     	        }
     	        pos++;
-    	        System.out.println("tree=" + ret);
+    	        //System.out.println("tree=" + ret);
     	    }
 	    }
+	    //System.out.println("buildTree() END itree=" + Arrays.toString(itree) + " ret=" + ret);
 	    return ret;
 	}
 	// get itree[pos], return null if invalid
@@ -122,8 +122,8 @@ class Solution {
                 for(int i=-1; i<c.rem.size(); i++){
                     Rec next = null;
                     if(i<0){
-                        if(!c.isLeftNull()){
-                            next = c.next(null);
+                        next = c.next(null);
+                        if(next!=null){
                             todo.add(next);
                         }
                     }else{
@@ -135,12 +135,65 @@ class Solution {
             //System.out.println("todo=" + todo);
             //System.out.println("done=" + done);
         }
+        //System.out.println("done=" + done);
         // 变换成树
         for(Rec r:done){
             ret.add(r.getTree());
         }
+        //System.out.println("ret=" + ret);
+        // 搜索树：左侧小，右侧大，整个子树都需要满足
+        List<TreeNode> ret2 = new ArrayList<>();
+        for(int i=0; i<ret.size(); i++){
+            TreeNode c = ret.get(i);
+            if(chk(c, null, null)){
+                ret2.add(c);
+            }
+        }
+        //System.out.println("ret2=" + ret2);
+        
+        // 长度测试
+        List<TreeNode> ret3 = new ArrayList<>();
+        for(int i=0; i<ret2.size(); i++){
+            TreeNode c = ret2.get(i);
+            int cnt = countNotNull(c);
+            if(cnt == n){
+                ret3.add(c);
+            }
+        }
+        //System.out.println("ret3=" + ret3);
+        return ret3;
+    }
+    // 检查是否符合 搜索树。true表示符合。
+    boolean chk(TreeNode nd, Integer uplimit, Integer downlimit){
+        boolean ret = false;
+        // 空可以
+        if(nd == null){
+            ret = true;
+        }else{
+            // 自己符合条件
+            if(   (uplimit   == null || uplimit  !=null && nd.val < uplimit)
+               && (downlimit == null || downlimit!=null && nd.val > downlimit)
+            ){
+                // 子树,上下限条件收紧
+                if(   chk(nd.left , nd.val, downlimit)
+                   && chk(nd.right, uplimit, nd.val)
+                ){
+                    ret = true;
+                }
+            }
+        }
+        //System.out.println("nd=" + nd + " up=" + uplimit + " dn=" + downlimit + " ret=" + ret);
         return ret;
     }
+    int countNotNull(TreeNode nd){
+	    int ret = 0;
+	    if(nd!=null){
+	        ret = 1;
+	        ret += nd.left==null ? 0 : countNotNull(nd.left);
+	        ret += nd.right==null ? 0 : countNotNull(nd.right);
+	    }
+	    return ret;
+	}
 }
 
 /**
@@ -151,66 +204,8 @@ class TreeNode {
     TreeNode left;
     TreeNode right;
     TreeNode(int x) { val = x; }
-}
-/*
-class TreeNode2 {
-    int val;
-    boolean nullflg = false;
-    TreeNode left;
-    TreeNode right;
-    TreeNode() { nullflg=true; }
-    TreeNode(int x) { val = x; }
-    TreeNode(int x, TreeNode lt, TreeNode rt) {
-        val = x;
-        left = lt;
-        right = rt;
-    }
-	TreeNode(Integer[] itree){
-	    //System.out.println("buildTree() itree=" + Arrays.toString(itree));
-	    if(itree.length>0){
-    	    Queue<TreeNode> todo = new ArrayDeque<TreeNode>();
-    	    val = itree[0];
-    	    int pos = 1; // 指示在 itree 中的位置
-    	    todo.add(this);
-    	    while(todo.size()>0){
-    	        TreeNode cnd = todo.remove(); // 现在处理中的 node
-    	        Integer cval = 0;
-    	        cval = getN(itree, pos);
-    	        // left
-    	        if(cval==null){
-    	            // cnd.left = null;
-    	        }else{
-    	            TreeNode tmp = new TreeNode(cval);
-    	            cnd.left = tmp;
-                    todo.add(tmp);
-    	        }
-    	        pos++;
-    	        cval = getN(itree, pos);
-
-    	        // right
-    	        if(cval==null){
-    	            // cnd.right = null;
-    	        }else{
-    	            TreeNode tmp = new TreeNode(cval);
-    	            cnd.right = tmp;
-                    todo.add(tmp);
-    	        }
-    	        pos++;
-    	        //System.out.println("tree=" + this);
-    	    }
-	    }
-	}
-	// get itree[pos], return null if invalid
-	private static Integer getN(Integer[] itree, int pos){
-	    Integer ret = null;
-	    if(itree==null || pos < 0 || pos >= itree.length){
-	        // ret = null;
-	    }else{
-	        ret = itree[pos];
-	    }
-	    return ret;
-	}
-	// 广度优先打印
+    
+    // 广度优先打印
     public String toString(){
         Queue<TreeNode> todo = new ArrayDeque<TreeNode>();
         String ret = "";
@@ -218,21 +213,22 @@ class TreeNode2 {
 
         while(todo.size()>0){
             TreeNode cnd = todo.remove(); // current node
-            if(cnd.nullflg){
-                ret += "null/";
+            if(cnd.val==-1){
+                ret += "_/";
             }else{
                 ret += cnd.val + "/";
             }
-            if(cnd.left==null && cnd.right==null){
+            //if(cnd.left==null && cnd.right==null){
                 // do nothing
-            }else{
+            //}else{
+            if(cnd.val!=-1){
                 if(cnd.left==null){
-                    todo.add(new TreeNode());
+                    todo.add(new TreeNode(-1));
                 }else{
                     todo.add(cnd.left);
                 }
                 if(cnd.right==null){
-                    todo.add(new TreeNode());
+                    todo.add(new TreeNode(-1));
                 }else{
                     todo.add(cnd.right);
                 }
@@ -240,13 +236,14 @@ class TreeNode2 {
         }
 
         return ret;
-    }	
+    }
 }
-*/
+
 public class Main
 {
 	public static void main(String[] args) {
 		t1();
+		//debug();
 	}
     static void tbase(int n){
 	    Solution o = new Solution();
@@ -257,7 +254,12 @@ public class Main
 	    return youret.equals(expect);
 	}
 	static void t1(){
-	    tbase(3);
+	    //tbase(3);
+	    tbase(4);
+	}
+	static void debug(){
+	    Rec r = new Rec(4);
+	    r.ls = Arrays.asList(new Integer[]{2, 1, 3, null, null, null, 4});
+	    r.getTree();
 	}
 }
-
