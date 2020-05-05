1122b575bb1ee0cc32a60c2820f3689943de4246
zxd <izayoi5776@gmail.com>
2020-05-05 09:11:14 +0900

    TreeNodeHelper

---
+++ 00_TreeNodeHelper.java
@@ -0,0 +1,102 @@
+import java.util.Arrays;
+import java.util.Queue;
+import java.util.ArrayDeque;
+
+/**
+ * Definition for a binary tree node.
+ * 这里删除掉 public 以方便和其他代码放在同一 java 文件中
+ */
+class TreeNode {
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x) { val = x; }
+}
+
+class TreeNodeHelper {
+	/**
+	 * 通过输入的配列初始化一棵二叉树
+	 * 例 tree = [7,4,3,null,null,6,19]
+	 * 配列是对树的广度搜索，从上到下，从左到右，空分支用null
+	 */
+	static TreeNode buildTree(Integer[] itree){
+	    TreeNode ret = null;
+	    //System.out.println("buildTree() itree=" + Arrays.toString(itree));
+	    if(itree.length>0){
+    	    Queue<TreeNode> todo = new ArrayDeque<TreeNode>();
+    	    ret = new TreeNode(itree[0]);
+    	    int pos = 1; // 指示在 itree 中的位置
+    	    todo.add(ret);
+    	    while(todo.size()>0){
+    	        TreeNode cnd = todo.remove(); // 现在处理中的 node
+    	        Integer cval = 0;
+    	        cval = getN(itree, pos);
+    	        // left
+    	        if(cval==null){
+    	            // cnd.left = null;
+    	        }else{
+    	            TreeNode tmp = new TreeNode(cval);
+    	            cnd.left = tmp;
+                    todo.add(tmp);
+    	        }
+    	        pos++;
+    	        cval = getN(itree, pos);
+
+    	        // right
+    	        if(cval==null){
+    	            // cnd.right = null;
+    	        }else{
+    	            TreeNode tmp = new TreeNode(cval);
+    	            cnd.right = tmp;
+                    todo.add(tmp);
+    	        }
+    	        pos++;
+    	        //System.out.println("tree=" + this);
+    	    }
+	    }
+	    return ret;
+	}
+	// get itree[pos], return null if invalid
+	private static Integer getN(Integer[] itree, int pos){
+	    Integer ret = null;
+	    if(itree==null || pos < 0 || pos >= itree.length){
+	        // ret = null;
+	    }else{
+	        ret = itree[pos];
+	    }
+	    return ret;
+	}
+    // 广度优先打印
+    public static String toString(TreeNode nd){
+        Queue<TreeNode> todo = new ArrayDeque<TreeNode>();
+        String ret = "";
+        todo.add(nd);
+
+        while(todo.size()>0){
+            TreeNode cnd = todo.remove(); // current node
+            if(cnd.val==-1){
+                ret += "_/";
+            }else{
+                ret += cnd.val + "/";
+            }
+            if(cnd.val!=-1){
+                if(cnd.left==null){
+                    todo.add(new TreeNode(-1));
+                }else{
+                    todo.add(cnd.left);
+                }
+                if(cnd.right==null){
+                    todo.add(new TreeNode(-1));
+                }else{
+                    todo.add(cnd.right);
+                }
+            }
+        }
+        return ret;
+    }
+}
+
+// call sample
+// TreeNode nd = TreeNodeHelper.buildTree(new Integer[]{2, 1, 3});
+// System.out.println(TreeNodeHelper.toString(nd));
+
